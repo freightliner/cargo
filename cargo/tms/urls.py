421538db278ff72f31f225e3b45bcfb58cf87742
saulario <saulario@ubuntu-18.04>
2018-08-12 15:10:55 +0200

    PrimeUI estable

--- cargo/tms/urls.py
+++ cargo/tms/urls.py
@@ -8,9 +8,16 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^signin/$', views.signin, name='signin'),
+        
+        
+        
         url(r'^cli/$', cliGest.index, name='cliGest'),
         url(r'^cli/(?P<id>[0-9]+)/$', cliGest.detail, name='cliGest.detail'),
         url(r'^cli/test/$', cliGest.test, name='cliGest.test'),
+        
+        url(r'^mm/$', views.mm, name='views-mm'),
+        
         url(r'^pai/$', paiGest.index, name='paiGest'),
         url(r'^sec/$', secGest.index, name='secGest'),
 ]
--- cargo/tms/views.py
+++ cargo/tms/views.py
@@ -1,4 +1,33 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'tms/index.html')
+
+def signin(request):
+    
+    username = request.POST['username']
+    password = request.POST['password']
+    
+    print('usuario %s, password %s' % (username, password))
+    
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'tms/mm.html')
+    else:
+        return render(request, 'tms/index.html')
+
+def signout(request):
+    logout(request)
+    return render(request, 'tms/index.html')
+
+
+
+
+
+
+
+
+def mm(request):
+    return render(request, 'tms/mm.html')
